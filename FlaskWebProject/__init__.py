591632f6077b597df50ad873890a685665927dc9
shubham0297 <36587738+shubham0297@users.noreply.github.com>
2020-06-23 21:48:58 +0530

    Initial Commit
    Initial Commit

---
+++ FlaskWebProject/__init__.py
@@ -0,0 +1,26 @@
+"""
+The flask application package.
+"""
+import logging
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+logHandler = logging.StreamHandler(stream=sys.stdout)
+logHandler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
+app.logger.addHandler(logHandler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Microblog startup')
+app.logger.info(header)
+
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import FlaskWebProject.views
